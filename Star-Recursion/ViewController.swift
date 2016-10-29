1d8f7bf92257d4a1ac2d2e2661ad287267eba54d
Daniel Mathews <dannyl.mathews@gmail.com>
2016-10-29 07:04:05 -0700

    v1

--- Star-Recursion/ViewController.swift
+++ Star-Recursion/ViewController.swift
@@ -13,7 +13,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        printStarsRecursively(number:10, str:"")
+        
+        
     }
+    
+    
+    func printStarsRecursively(number:Int, str:String){
+        
+        if number == 0 {
+            return
+        }
+        
+        let newStr = str + "*"
+        printStarsRecursively(number: number-1, str: newStr)
+        print(newStr)
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
